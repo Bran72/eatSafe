6d1e09a47164bb42530f4149e474ee7b0457f2a5
Brandon Leininger <brandon.leininger@icloud.com>
2019-11-14 09:35:08 +0100

    :wrench: Updating workflow and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
 
-exports.default = defaultTask
+const srcJS = './js';
+const srcCSS = './css';
+const destination = './src';
+
+gulp.task('css-dev', function () {
+    return gulp.src(srcCSS + '/css_framework/sass/main.scss')
+        .pipe(plugins.sass())
+        .pipe(plugins.autoprefixer({
+            browsers: ['last 2 versions', '> 2%']
+        }))
+        .pipe(gulp.dest(destination + '/assets/css/'));
+});
+
+gulp.task('css-prod', function () {
+    return gulp.src(srcCSS + '/css_framework/sass/main.scss')
+        .pipe(plugins.sass())
+        .pipe(plugins.autoprefixer({
+            browsers: ['last 2 versions', '> 2%']
+        }))
+        .pipe(plugins.csso())
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(destination + '/assets/css/'));
+});
