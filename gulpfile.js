abd9a01f5a2db76a1a5796ed0afa46e501aa602d
Brandon Leininger <brandon.leininger@icloud.com>
2019-11-28 11:17:38 +0100

    Update gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('css-dev', function () {
 });
 
 gulp.task('css-prod', function () {
-    return gulp.src(srcCSS + '/custom.scss')
+    return gulp.src(['./assets/css/custom.scss', './assets/css/piodjio.css'])
         .pipe(plugins.sass())
         .pipe(plugins.autoprefixer())
         .pipe(plugins.rename({
@@ -24,10 +24,11 @@ gulp.task('css-prod', function () {
 });
 
 gulp.task('watch:css', () => {
-    gulp.watch(['./assets/css/custom.scss', './assets/css/piodjio.css'], gulp.series('css-prod'));
+    gulp.watch(['./assets/css/*.scss','./assets/css/*.ccs' ], gulp.series('css-prod'));
 });
 
 gulp.task('images', function(){
+    console.log('images')
     return gulp.src('./assets/img/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./build/assets/img'))
@@ -45,4 +46,4 @@ gulp.task('default', () => {
     }
 });*/
 
-gulp.task('default', gulp.parallel('watch:css','images'));
+gulp.task('default', gulp.parallel('css-prod', 'watch:css', 'images'));
