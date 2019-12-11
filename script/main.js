0e46382809a7f394571f2572e56996c2b7473e30
Pierre Darrieutort <p.darrieutort@outlook.fr>
2019-12-11 22:15:43 +0100

    Menu intégré + Sign page undisplayed

--- script/main.js
+++ script/main.js
@@ -1,7 +1,21 @@
-document.querySelectorAll( '#burgerMenu input' ).forEach( el => {
+document.querySelectorAll( '#SignInterface input' ).forEach( el => {
     el.addEventListener( 'input', () => {
         el.value.length !== 0
             ? el.classList.add( 'filled' )
             : el.classList.remove( 'filled' )
     } )
+} )
+
+const burger = document.getElementById( "burger" );
+
+function toggleIcon() {
+    burger.classList.contains( 'actif' )
+        ? burger.textContent = "⛌"
+        : burger.textContent = "☰"
+}
+toggleIcon()
+
+burger.addEventListener( "click", function () {
+    burger.classList.toggle( 'actif' )
+    toggleIcon()
 } )
